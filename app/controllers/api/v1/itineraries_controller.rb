19150726aff50e7c7948c636e6c102835be85658
github username <github email address>
2017-12-04 15:35:59 -0500

    added controllers and routes

---
+++ app/controllers/api/v1/itineraries_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::ItinerariesController < ApplicationController
+
+  before_action :set_itinerary, only: [:show, :update, :destroy]
+  # GET /itineraries
+  def index
+    @itineraries = Itinerary.all
+    render json: @itineraries
+  end
+
+  # GET itineraries/1
+  def show
+    render json: @itinerary
+  end
+
+  # POST /itineraries
+  def create
+    @itinerary = Itinerary.new(itinerary_params)
+    if @itinerary.save
+      render json: @itinerary,status: :created, location: @itinerary
+    else
+      render json: @itinerary.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /itineraries/1
+  def update
+    @itinerary = Itinerary.find(params[:id])
+    @itinerary.update(itinerary_params)
+    if @itinerary.save
+      render json: @itinerary
+    else
+      render json: @itinerary.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @itinerary.destroy
+  end
+
+  private
+  def set_itinerary
+    @itinerary = Itinerary.find(params[:id])
+  end
+
+  def itinerary_params
+    params.require(:itinerary).permit(:user_id, :name, :description, :notes, :start_date, :end_date, :avatar)
+  end 
+end
---
+++ app/controllers/api/v1/locations_controller.rb
@@ -0,0 +1,48 @@
+class Api::V1::LocationsController < ApplicationController
+
+  before_action :set_location, only: [:show, :update, :destroy]
+  # GET /locations
+  def index
+    @locations = Location.all
+    render json: @locations
+  end
+
+  # GET locations/1
+  def show
+    render json: @location
+  end
+
+  # POST /location
+  def create
+    @location = Location.new(location_params)
+    if @location.save
+      render json: @location,status: :created, location: @location
+    else
+      render json: @location.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /itineraries/1
+  def update
+    @location = Location.find(params[:id])
+    @location.update(location_params)
+    if @location.save
+      render json: @location
+    else
+      render json: @location.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @location.destroy
+  end
+
+  private
+  def set_location
+    @location = Location.find(params[:id])
+  end
+
+  def location_params
+    params.require(:location).permit(:name, :street_address, :city, :state, :country, :postal_code, :description, :avatar)
+  end
+end
