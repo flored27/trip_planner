9ec5029e724ddbb221cc3045bf28776843863b35
Abe Rudy <aberudy@gmail.com>
2017-12-05 09:05:13 -0500

    JSON output formatted properly, ready for front-end

--- app/serializers/itinerary_serializer.rb
+++ app/serializers/itinerary_serializer.rb
@@ -1,3 +1,21 @@
 class ItinerarySerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :name
+  attributes :id, :user_id, :description, :name, :things_to_do, :destinations
+
+  def destinations
+    object.locations.map do |location|
+      {id:location.id,
+        name: location.name,
+        street_address: location.street_address,
+        city: location.city,
+        state: location.state,
+        zip: location.postal_code}
+      end
+  end
+
+  def things_to_do
+    object.locations.count
+  end
+
+  private
+
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,14 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email
+  attributes :id, :name, :email, :user_trips
+
+  def user_trips
+    object.itineraries.map do |trip|
+      {name: trip.name,
+        description: trip.description,
+        start_date: trip.start_date,
+        end_date: trip.end_date}
+      end
+  end
+
+
 end
