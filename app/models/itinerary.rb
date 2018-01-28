4b287d70e6f01c0dbf76ead28038a6027276d8b7
github username <lina.rudashevski@gmail.com>
2018-01-28 18:09:28 -0500

    removed has attached file

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -3,6 +3,4 @@ class Itinerary < ApplicationRecord
   has_many :stops
   has_many :locations, through: :stops
 
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -3,7 +3,4 @@ class Location < ApplicationRecord
   has_many :itineraries, through: :stops
   has_many :users, through: :itineraries
 
-
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
